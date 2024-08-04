5faa3f0869b78c40cccd284ffc1813c08e649dc5
SmritiGuragain <smritiguragain111@gmail.com>
2024-08-04 16:23:29 +0545

    hero section
    Navbar and hero section done

---
+++ assests/javascript/index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var options = {
+        strings: ["passionate developer", "creative designer", "problem solver"],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true
+    };
+
+    var typed = new Typed(".typed-text", options);
+});
